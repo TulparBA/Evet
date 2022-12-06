c63e05166276d4ebbf8e98c966c39c8340a76c29
Yalınsak <80483061+TulparBA@users.noreply.github.com>
2022-12-06 13:45:36 +0300

    Hata Verdirme

---
+++ Hata.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNumara_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNumara.Text)) 
+            {
+                e.Cancel = true;
+                errorProvider1.SetError(txtNumara, "Numara Boş Bıraklılamaz");
+            }
+            else 
+            {
+                errorProvider1.SetError(txtNumara, " ");
+            }
+        }
+    }
+}
