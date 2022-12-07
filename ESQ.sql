1afd147848f08b289a5934d1751d896db8133037
Yalınsak <80483061+TulparBA@users.noreply.github.com>
2022-12-07 10:39:40 +0300

    SQL

---
+++ ESQ.sql
@@ -0,0 +1,12 @@
+select @@CONNECTIONS as 'Number of Login Attemps'
+
+SELECT * FROM Ogrenci
+if(@@ERROR<>0)
+print 'Error Found'
+else
+print'Error Not Found'
+
+select @@IDLE as 'Idle Milliseconds Time'
+select @@LANGUAGE as 'Language'
+select min(Copies) from [BooksLibrary].[dbo].[TotalBooks]
+select sum(Cinsiyet)
